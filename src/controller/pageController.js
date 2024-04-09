0a132f31c22e555b6178935034104894d1e981be
Leonardo m <leonardomaiaarruda@gmail.com>
2024-04-09 10:58:20 -0300

    pages/page esta ok

--- src/controller/pageController.js
+++ src/controller/pageController.js
@@ -1,11 +1,38 @@
 import express from "express";
 
 export const home = (req, res)=>{
-    res.send("home do controller");
+    res.render('pages/page',{
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
+    
 };
 
-export const dogs = (req, res)=>{};
+export const dogs = (req, res)=>{   
+    res.render('pages/page',{
+        banner: {
+            title: 'Todos os Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
+};
 
-export const cats = (req, res)=>{};
+export const cats = (req, res)=>{
+    res.render('pages/page',{
+        banner: {
+            title: 'Todos os Gatos',
+            background: 'allanimals.jpg'
+        }
+    });
+};
 
-export const fishes = (req, res)=>{};
+export const fishes = (req, res)=>{
+    res.render('pages/page',{
+        banner: {
+            title: 'Todos os Peixes',
+            background: 'allanimals.jpg'
+        }
+    });
+};
